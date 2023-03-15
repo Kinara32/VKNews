f9e28974f4e60c463e9b0b19f99fc710247140ee
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-03-15 22:37:10 +0300

    Initial Commit

---
+++ VKNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKNews
+//
+//  Created by Matvei Bykadorov on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
