ba5dbf73b328089bb69dec7271f15c37d5bf1d48
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-03-29 02:49:38 +0300

    constraint programmatically

--- VKNews/NewsFeed/Cell/NewsFeedCodeCell.swift
+++ VKNews/NewsFeed/Cell/NewsFeedCodeCell.swift
@@ -12,9 +12,36 @@ final class NewsFeedCodeCell: UITableViewCell {
     
     static let reuseId = "NewsFeedCodeCell"
     
+    let cardView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemRed
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .cyan
+        addSubview(cardView)
+
+//        cardView.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
+//        cardView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12).isActive = true
+//        cardView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+//        cardView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+        
+//        cardView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+//        cardView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//        cardView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        
+//        cardView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive = true
+//        cardView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive = true
+//        cardView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+//        cardView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        cardView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        cardView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        cardView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        cardView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 1/2).isActive = true
     }
     
     required init?(coder: NSCoder) {
